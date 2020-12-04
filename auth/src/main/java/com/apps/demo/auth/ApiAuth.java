b777b0999c5eedc8cbaa607b66c85ec213d94a4f
jayeshta <jayeshta9889@gmail.com>
2020-12-04 14:35:30 +0530

    Initial commit

---
+++ auth/src/main/java/com/apps/demo/auth/ApiAuth.java
@@ -0,0 +1,8 @@
+package com.apps.demo.auth;
+
+public class ApiAuth {
+
+    public static final String Url = "https://playdevelopers.com/api/android/apps/json/auth";
+    public static final String app_auth = "FGDRG32DS2GVD31VEG1XD2V36EF3D2BV";
+
+}
